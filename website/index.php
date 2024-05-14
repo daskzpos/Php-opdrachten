82f7e3a3bdbfa06f75ee9164e75b0ad8774ff0b2
daskzpos <dashapostma@gmail.com>
2024-05-14 11:02:21 +0200

    update

--- website/index.php
+++ website/index.php
@@ -10,7 +10,7 @@ echo $c;
 $auto =array("peugeot", "opel", "ferrari");
 echo $auto[2];
 
-$objectauto = array('kleur' => 'geel', 'snelheid'  => 200, 'zitplaatsen' =>4);
+$objectauto = array('kleur' => 'geel', 'snelheid'  => 0, 'zitplaatsen' =>4);
 echo "ik heb een auto die is $objectauto[kleur]. Hij rijdt $objectauto[snelheid] er uur en de auto biedt plaats voor $objectauto[zitplaatsen].";
 
 $naam = "Dasha";
@@ -19,4 +19,18 @@ $woonplaats = "Almere";
 
 echo "Mijn naam is $naam. ik ben geboren op $geboortedatum en woon in $woonplaats.";
 
+if ($objectauto['kleur'] == 'rood'){
+echo "ik ben een rode auto!";
+} else {
+    echo "ik ben een auto met een andere kleur.";
+}
 
+if ($objectauto['snelheid'] < 50 && $objectauto['snelheid'] > 0){
+    echo "dit is een langzame auto";
+} elseif ($objectauto['snelheid'] >= 50 && $objectauto['snelheid'] <= 100){
+    echo "dit is een normale auto";
+} elseif ($objectauto['snelheid'] > 100){
+    echo "dit is een snelle auto";
+} else {
+    echo "deze auto rijdt niet";
+}
