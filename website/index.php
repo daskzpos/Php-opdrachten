e0310408e783e51b4861911603eafe977d443f63
daskzpos <dashapostma@gmail.com>
2024-05-14 10:39:47 +0200

    update

--- website/index.php
+++ website/index.php
@@ -1,3 +1,22 @@
 <?php
-    $test = 'Hello world';
-    echo $test; 
+$txt = "Hello World";
+echo $txt;
+
+$a = 3;
+$b = 7;
+$c = $a + $b;
+echo $c;
+
+$auto =array("peugeot", "opel", "ferrari");
+echo $auto[2];
+
+$objectauto = array('kleur' => 'geel', 'snelheid'  => 200, 'zitplaatsen' =>4);
+echo "ik heb een auto die is $objectauto[kleur]. Hij rijdt $objectauto[snelheid] er uur en de auto biedt plaats voor $objectauto[zitplaatsen].";
+
+$naam = "Dasha";
+$geboortedatum = "27-01-2004";
+$woonplaats = "Almere";
+
+echo "Mijn naam is $naam. ik ben geboren op $geboortedatum en woon in $woonplaats.";
+
+
