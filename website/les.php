8c1ef584bb04406ad79a9de239663f43112c93cb
daskzpos <dashapostma@gmail.com>
2024-05-16 11:15:25 +0200

    update

--- website/les.php
+++ website/les.php
@@ -1,2 +1,5 @@
 <?php
-$dasha = array('leeftijd' => '20', 'woonplaats' => 'Almere');
+//$dasha = array('leeftijd' => '20', 'woonplaats' => 'Almere');
+//echo "$dasha[leeftijd]";
+
+$array= array('naam' => 'niek'); array('naam' => 'jesse'); array('naam' => 'piet');
