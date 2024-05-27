29ea8d38082e1a95cb100d94d4d8897735038b98
daskzpos <dashapostma@gmail.com>
2024-05-27 14:07:31 +0200

    update

--- website/fullstack.php
+++ website/fullstack.php
@@ -1,25 +1,22 @@
 <?php
 
-require("database.php");
+require ('database.php');
+// $sql1 = "CREATE DATABASE MyDB" ;
+// if($conn->query($sql1) === true){
+//     echo "Database created succesfully";
+// } else {
+//     echo "Error creating database" .$conn->error ;
+// }
 
-$sql1 = "create database MyDB";
+// $sql = "CREATE TABLE  MyGuests (
+//     id INT(6) unsigned auto_increment primary key,
+//     bandname varchar(30) not null
+//     )";
 
-if($conn->query($sql1) === true){
-    echo "Database created successfully";
-} else {
-    echo "Error creating database: " .$conn->error;
-}
+// if($conn->query($sql) === true) {
+//     echo "Table created successfully";
+// }else{
+//     echo "Error creating database: " . $conn->error;
+// }
 
-$sql = "create table MyGuests (
-    id INT(6) unsigned auto_increment primary key,
-    firstname varchar(30) not null,
-    lastname varchar(30) not null,
-    email varchar(50),
-    reg_date tomestamp default current_timestamp on update current_timestamp
-    )";
-
-if($conn->query($sql) === true) {
-    echo "Table created successfully";
-}else{
-    echo "Error creating database: " . $conn->error;
-}
+?>
